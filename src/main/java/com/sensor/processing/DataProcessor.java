0afcdfeb197c4ce1b20159104630f2558fb29744
osman <shaikhosman02@gmail.com>
2025-02-28 17:33:00 +0530

    used streams for efficent processing

--- src/main/java/com/sensor/processing/DataProcessor.java
+++ src/main/java/com/sensor/processing/DataProcessor.java
@@ -1,55 +1,47 @@
 package com.sensor.processing;
 
 import com.sensor.data.SensorData;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class DataProcessor {
 
     public Map<String, Map<String, Double>> calculateMonthlyAverages(List<SensorData> sensorData) {
-        Map<String, Map<String, Double>> monthlyAverages = new HashMap<>();
-
-        for (SensorData data : sensorData) {
-            String sensorType = data.getSensorType();
-            String month = data.getMonth(); // Use the month field
-            double value = data.getValue();
-
-            monthlyAverages.computeIfAbsent(sensorType, k -> new HashMap<>())
-                    .merge(month, value, (oldValue, newValue) -> (oldValue + newValue) / 2);
-        }
-
-        return monthlyAverages;
+        return sensorData.stream()
+                .collect(Collectors.groupingBy(
+                        SensorData::getSensorType,
+                        Collectors.groupingBy(
+                                SensorData::getMonth,
+                                Collectors.averagingDouble(SensorData::getValue)
+                        )
+                ));
     }
 
     public Map<String, Map<String, Double>> calculateMonthlyMax(List<SensorData> sensorData) {
-        Map<String, Map<String, Double>> monthlyMax = new HashMap<>();
-
-        for (SensorData data : sensorData) {
-            String sensorType = data.getSensorType();
-            String month = data.getMonth(); // Use the month field
-            double value = data.getValue();
-
-            monthlyMax.computeIfAbsent(sensorType, k -> new HashMap<>())
-                    .merge(month, value, Math::max);
-        }
-
-        return monthlyMax;
+        return sensorData.stream()
+                .collect(Collectors.groupingBy(
+                        SensorData::getSensorType,
+                        Collectors.groupingBy(
+                                SensorData::getMonth,
+                                Collectors.collectingAndThen(
+                                        Collectors.maxBy(Comparator.comparingDouble(SensorData::getValue)),
+                                        opt -> opt.map(SensorData::getValue).orElse(0.0)
+                                )
+                        )
+                ));
     }
 
     public Map<String, Map<String, Double>> calculateMonthlyMin(List<SensorData> sensorData) {
-        Map<String, Map<String, Double>> monthlyMin = new HashMap<>();
-
-        for (SensorData data : sensorData) {
-            String sensorType = data.getSensorType();
-            String month = data.getMonth(); // Use the month field
-            double value = data.getValue();
-
-            monthlyMin.computeIfAbsent(sensorType, k -> new HashMap<>())
-                    .merge(month, value, Math::min);
-        }
-
-        return monthlyMin;
+        return sensorData.stream()
+                .collect(Collectors.groupingBy(
+                        SensorData::getSensorType,
+                        Collectors.groupingBy(
+                                SensorData::getMonth,
+                                Collectors.collectingAndThen(
+                                        Collectors.minBy(Comparator.comparingDouble(SensorData::getValue)),
+                                        opt -> opt.map(SensorData::getValue).orElse(0.0)
+                                )
+                        )
+                ));
     }
 }
